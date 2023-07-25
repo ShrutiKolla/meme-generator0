ae11e2673c4d0600ccd6eecd4795e8662712024f
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-25 18:17:18 +0530

    .

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -2,16 +2,13 @@
 // Components
 import Nav from './Components/Nav.jsx'
 import Meme from './Components/Meme.jsx'
-import {meme}from './Components/MemeDb.js'
 
 function App() {
-  const memes = meme.data.memes;
-  let memeLen = memes.length
-  console.log(Math.floor(memeLen*Math.random()));
+
   return (
     <>
       <Nav/>  
-      <Meme memes = {memes}/>   
+      <Meme />   
     </>
   )
 }
--- meme-generator/src/Components/Meme.jsx
+++ meme-generator/src/Components/Meme.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// imgs
+// dbs
+import memeData from "./MemeDb.json";
 
 // css
 import mainCss from '../assets/css/main.module.css';
-export default function Meme(props) {
-    const [imgUrl, setImgUrl] = React.useState("");
+
+export default function Meme() {
+    /**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
+
+    const [meme, setMeme] = React.useState(
+        {
+            topText : "",
+            bottomText : "",
+            randomImage : "http://i.imgflip.com/1bij.jpg"
+        }
+    )
+
+    const [allMemeImages, setAllMemeImages] = useState(memeData.data.memes);
+
     const handleClick = function() {
-        const memeLen = props.memes.length;
-        const idx = Math.floor(memeLen*Math.random());
-        setImgUrl(props.memes[idx].url);
+        const idx = Math.floor(allMemeImages.length * Math.random());
+        setMeme(prevMeme => ({...prevMeme, randomImage : allMemeImages[idx].url}))
+        console.log(allMemeImages[idx].name)
     }
+
     return (
         <main>
             <div className={mainCss.form}>
@@ -26,7 +55,7 @@ export default function Meme(props) {
                 />
                 <button onClick={handleClick} className={mainCss.form__button}>Get a new meme image  🖼</button>
                 <div className={mainCss.image}>
-                    {imgUrl && <img src={imgUrl} alt="" />}
+                    <img src={meme.randomImage} alt="" />
                     
                 </div>
             </div>
