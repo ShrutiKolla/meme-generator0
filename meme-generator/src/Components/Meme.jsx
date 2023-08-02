0a934c6411fa3ee2441f84979309b9c52e5301c3
me-mystic <Shrutikolla.2002@gmail.com>
2023-08-02 15:18:03 +0530

    .

--- meme-generator/src/Components/Meme.jsx
+++ meme-generator/src/Components/Meme.jsx
@@ -40,23 +40,39 @@ export default function Meme() {
         console.log(allMemeImages[idx].name)
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setMeme(prev => (
+            {
+                ...prev,
+                [name]:value
+            }
+        ))
+    }
     return (
         <main>
             <div className={mainCss.form}>
                 <input 
                     type="text" 
                     placeholder='Top text'
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                     className={mainCss.form__input}
                 />
                 <input 
                     type="text" 
                     placeholder='Bottom text'
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                     className={mainCss.form__input}
                 />
                 <button onClick={handleClick} className={mainCss.form__button}>Get a new meme image  🖼</button>
-                <div className={mainCss.image}>
+                <div className={mainCss.meme}>
                     <img src={meme.randomImage} alt="" />
-                    
+                    <h2 className={mainCss.meme_top}>{meme.topText}</h2>
+                    <h2 className={mainCss.meme_bottom}>{meme.bottomText}</h2>
                 </div>
             </div>
         </main>
